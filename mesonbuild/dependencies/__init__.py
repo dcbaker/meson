ff65070d00212e3b9805140fc39d0466efbd076a
Dylan Baker <dylan@pnwbakers.com>
2021-01-20 15:01:09 -0800

    dependencies: Add docstring explaining how to write a system dependency
    This is a useful thing to document. I wasn't really sure where to put
    it, but since it's developer oriented I figured in the code itself was
    probably more useful and more likely to be seen than in the markdown
    that generates the website.

--- mesonbuild/dependencies/__init__.py
+++ mesonbuild/dependencies/__init__.py
@@ -32,6 +32,173 @@ from .misc import (
 from .platform import AppleFrameworks
 from .ui import GnuStepDependency, Qt4Dependency, Qt5Dependency, WxDependency, gl_factory, sdl2_factory, vulkan_factory
 
+"""Dependency representations and discovery logic.
+
+Meson attempts to largely abstract away dependency discovery information, and
+to encapsulate that logic itself so that the DSL doesn't have too much direct
+information. There are some cases where this is impossible/undesirable, such
+as the `get_variable()` method.
+
+Meson has four primary dependency types:
+  1. pkg-config
+  2. apple frameworks
+  3. cmake
+  4. system
+
+Plus a few more niché ones.
+
+When a user calls `dependency('foo')` meson creates a list of candidates, and
+tries those candidates in order to find one that matches the criteria
+provided by the user (such as version requirements, or optional components
+that are required.)
+
+Except to work around bugs or handle odd corner cases, pkg-config and cmake
+generally just work™, though there are exceptions. Most of this package is
+concerned with dependencies that don't (always) provide cmake and/or
+pkg-config files.
+
+For these cases one needs to write a `system` dependency. These dependencies
+descend directly from `ExternalDependency`, in their constructor they
+manually set up the necissary link and compile args (and additional
+dependencies as necessary).
+
+For example, imagine a dependency called Foo, it uses an environment variable
+called $FOO_ROOT to point to it's install root, which looks like this:
+```txt
+$FOOROOT
+→ include/
+→ lib/
+```
+To use Foo, you need it's include directory, and you need to link to
+`lib/libfoo.$lib-end`.
+
+You could write code that looks like:
+
+```python
+class FooSystemDependency(ExternalDependency):
+
+    def __init__(self, name: str, environment: 'Environment', kwargs: T.Dict[str, T.Any]):
+        super().__init__(name, environment, kwargs)
+        root = os.environ.get('FOO_ROOT')
+        if root is None:
+            mlog.debug('$FOO_ROOT is unset.')
+            self.is_found = False
+            return
+
+        lib = self.clib_compiler.find_library('foo', environment, [os.path.join(root, 'lib')])
+        if lib is None:
+            mlog.debug('Could not find lib.')
+            self.is_found = False
+            return
+
+        self.compile_args.append(f'-I{os.path.join(root, "include")}')
+        self.link_args.append(lib)
+        self.is_found = True
+```
+
+This code will look for 'FOO_ROOT` in the environement, handle FOO_ROOT being
+undefined gracefully, then set the it's `compile_args` and `link_args`
+gracefully. It will also gracefully handle not finding the required lib
+(hopefully that doesn't happen, but it could if say the lib is only static
+and shared linking is requested).
+
+There are a couple of thigns about this that still aren't ideal. For one, we
+don't want to be reading random environment variables at this point. Those
+should actually be added to `envconfig.Properties` and read in
+`environment.Environment._set_default_properties_from_env` (see how
+BOOST_ROOT is handled). So now that becomes:
+
+```python
+class FooSystemDependency(ExternalDependency):
+
+    def __init__(self, name: str, environment: 'Environment', kwargs: T.Dict[str, T.Any]):
+        super().__init__(name, environment, kwargs)
+        root = environment.properties[self.for_machine].foo_root
+        if root is None:
+            mlog.debug('foo_root is unset.')
+            self.is_found = False
+            return
+
+        lib = self.clib_compiler.find_library('foo', environment, [os.path.join(root, 'lib')])
+        if lib is None:
+            mlog.debug('Could not find lib.')
+            self.is_found = False
+            return
+
+        self.compile_args.append(f'-I{os.path.join(root, "include")}')
+        self.link_args.append(lib)
+        self.is_found = True
+```
+
+This is nicer in a couple of ways. First we can properly cross compile as we
+are allowed to set FOO_ROOT for both the build and host machines, it also
+means that users can override this in their machine files, and finally if
+that environment variables changes durring a meson reconfigure meson won't
+re-read it.
+
+We can still make this better by handling the `static` keyword
+```python
+class FooSystemDependency(ExternalDependency):
+
+    def __init__(self, name: str, environment: 'Environment', kwargs: T.Dict[str, T.Any]):
+        super().__init__(name, environment, kwargs)
+        root = environment.properties[self.for_machine].foo_root
+        if root is None:
+            mlog.debug('foo_root is unset.')
+            self.is_found = False
+            return
+
+        static = kwargs.get('static', False)
+        static = mesonlib.LibType.STATIC if kwargs.get('static', False) else mesonlib.LibType.SHARED
+        lib = self.clib_compiler.find_library(
+            'foo', environment, [os.path.join(root, 'lib')], libtype=static)
+        if lib is None:
+            mlog.debug('Could not find lib.')
+            self.is_found = False
+            return
+
+        self.compile_args.append(f'-I{os.path.join(root, "include")}')
+        self.link_args.append(lib)
+        self.is_found = True
+```
+
+Now we meson will figure out wither it should be finding `libfoo.so` or
+`libfoo.a` (or the platform specific names). Things are looking pretty good
+now, so it can be added to the `packages` dict below
+```python
+packages.udate({
+    'foo': FooSystemDependency,
+})
+```
+
+Now, what if foo also provides pkg-config, but it's only shipped on Unices,
+or only inlucded in very recent versions? We can use the `DependencyFactory`
+class:
+```python
+foo_factory = DependencyFactory(
+    'foo',
+    [DependencyMethods.PKGCONFIG, DependencyMethods.SYSTEM],
+    system_class=FooSystemDependency,
+)
+```
+This is a helper function that will generate a default pkg-config based
+dependency, and use the `FooSystemDependency` as well. You would then add the
+`foo_factory` to packages instead
+```python
+packages.udate({
+    'foo': foo_factory,
+})
+```
+
+If you have a dependency that is very complicated, (such as having multiple
+implementations) you may need to write your own factory function. There are a
+number of examples in this pacakge.
+
+_Note_ before we moved to factory functions it was common to use an
+`ExternalDependency` class that would instantiate different types of
+dependencies and hold the one it found. There are a number of drawbacks to
+this approach, and no new dependencies should do this.
+"""
 
 # This is a dict where the keys should be strings, and the values must be one
 # of:
