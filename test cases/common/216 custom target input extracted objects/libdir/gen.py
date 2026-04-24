2f79060b78c2ec63b72f379504ef6da3bc03dd14
Dylan Baker <dylan@pnwbakers.com>
2026-04-24 12:59:44 -0700

    tests/common/216: clean up the python scripts
    Get some better debugging messages, and clean up warnings. I'm trying to
    debug this test only on Windows

--- test cases/common/216 custom target input extracted objects/libdir/gen.py
+++ test cases/common/216 custom target input extracted objects/libdir/gen.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
 import sys
-with open(sys.argv[1], 'r') as f:
+
+if (l := len(sys.argv)) != 4:
+    print('Got the wrong number of arguments. Got', l, 'but expected 4', file=sys.stderr)
+
+with open(sys.argv[1], 'r', encoding='utf-8') as f:
     for l in f:
         l = l.rstrip()
         print(l.replace(sys.argv[2], sys.argv[3]))
